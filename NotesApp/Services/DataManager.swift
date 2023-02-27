0f1309ba373e9d4d98fcbd54c4b16841f12bfe67
Polyus <polyus@mail.ru>
2023-02-27 22:21:42 +0500

    add tableViewController + NavigationBar add CoreData add StorageManager add TempDataManager

--- NotesApp/Services/DataManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DataManager.swift
-//  NotesApp
-//
-//  Created by Сергей Поляков on 27.02.2023.
-//
-
-import Foundation
---
+++ NotesApp/Services/TempDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  TempDataManager.swift
+//  NotesApp
+//
+//  Created by Сергей Поляков on 27.02.2023.
+//
+
+import Foundation
+
+class TempDataManager {
+    static let shared = TempDataManager()
+    
+    private init() {}
+    
+    func create() {
+        
+    }
+}
